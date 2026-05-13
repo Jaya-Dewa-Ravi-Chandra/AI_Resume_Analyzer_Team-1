0cc60ecb6c696682b2d9c8157747ed52b5c584ab
Jaya-Dewa-Ravi-Chandra <jayadewaravichandra@gmail.com>
2026-05-13 23:11:18 +0530

    Update cloudinary.js

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -1,10 +1,19 @@
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+
+dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
+
+console.log("CLOUDINARY CHECK:");
+console.log(process.env.CLOUD_NAME);
+console.log(process.env.CLOUD_API_KEY ? "API KEY FOUND" : "NO API KEY");
+console.log(
+  process.env.CLOUD_API_SECRET ? "SECRET FOUND" : "NO SECRET",
+);
 
 export default cloudinary;
